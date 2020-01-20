07889f9f9078c694f1d7d966c1dc5fbc2e1925c5
Will Patterson <wpatterson@witekio.com>
2020-01-19 20:00:12 -0800

    testing

--- yocto/container-runme.sh
+++ yocto/container-runme.sh
@@ -12,41 +12,16 @@ git config --global user.name "Will Patterson"
 git config --global user.email "wpatterson@witekio.com"
 
 cd sources
-test -d .repo || repo init -u git@github.com:willpnw/raspberry-manifest.git
+test -d .repo || repo init -u git@github.com:willpnw/raspberry.git
 
 repo sync
 
-# ONE TIME REPO SETUP
-#repo init -u git@bitbucket.org:adeneo-embedded/tablesafe-manifests.git
-
-# ONE TIME SETUP LOCAL MANIFEST
-#mkdir .repo/local_manifests/
-#cat <<EOF > .repo/local_manifests/local_manifest.xml
-#<?xml version="1.0" encoding="UTF-8"?>
-#<manifest>
-#    <remove-project name="tablesafe-meta-tablesafe.git" />
-#    <project name="tablesafe-meta-tablesafe.git" remote="witekio" path="meta-tablesafe" revision="sbach/develop"/>
-#</manifest>
-#EOF
-
-# ONE TIME REPO SETUP
-#repo sync
-
-# ONE TIME SETUP THE LOCAL CONF
-#cat <<EOF >> conf/local.conf
-#
-## Override Git branches used for various packages.
-#KBRANCH = "broadcom/ga-3.7-bcm583xx"
-#SRCBRANCH_pn-boot1 = "broadcom/ga-3.7-bcm583xx"
-#SRCBRANCH_pn-secimage-native = "broadcom/ga-3.7-bcm583xx"
-#SRCBRANCH_pn-u-boot-tablesafe = "broadcom/ga-3.7-bcm583xx"
-#
-#INHERIT += "buildhistory"
-#BUILDHISTORY_COMMIT = "1"
-#EOF
-
-
 # SETUP THE BUILD ENV AND BITBAKE SYSTEM IMAGE
 set +x
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 echo ". sources/oe-init-build-env build"
 echo "bitbake core-image-base"
